29a1671d00317bcfd956772bd0730608b1c17221
Mohamed-Ghanem155 <promopcbig458@gmail.com>
2026-07-19 19:17:26 +0300

    fix: add timeouts to firestore calls in flutter web to prevent infinite loading

--- flutter_web_build/flutter_bootstrap.js
+++ flutter_web_build/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "680682809"
+    serviceWorkerVersion: "4195635359"
   }
 });

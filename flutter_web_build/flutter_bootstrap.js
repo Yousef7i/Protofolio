44229ae016ff966ad1648606cb20dfc34cb95171
Mohamed-Ghanem155 <promopcbig458@gmail.com>
2026-07-31 23:57:13 +0300

    update: portfolio data JSON with new CV information

--- flutter_web_build/flutter_bootstrap.js
+++ flutter_web_build/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "88449369"
+    serviceWorkerVersion: "1252835221"
   }
 });

556874df685366d9dd1af9b1dd04955c352cc5d0
Mohamed-Ghanem155 <promopcbig458@gmail.com>
2026-07-31 21:12:55 +0300

    feat: updated demo button to have pulse and shimmer animation

--- flutter_web_build/flutter_service_worker.js
+++ flutter_web_build/flutter_service_worker.js
@@ -32,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d187f5194cc20f9ab6b41bfc0e8fe590
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "cv.pdf": "868930ed93a66ad03bd7de810a2dcdce",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "61513deea615c9172ca2326e1f9eeb86",
+"flutter_bootstrap.js": "fea9c818d3226ce76ba1a0851ee60ee7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "2575e728eb872e4cf4bf87c382d9c26c",
 "/": "2575e728eb872e4cf4bf87c382d9c26c",
-"main.dart.js": "403bfb7c69f9191a4c359513b53df126",
+"main.dart.js": "1a26811c6804e0c93b1774e8b4ec2580",
 "manifest.json": "5e6899ef49e7405ac47ed4c97b6a6139",
 "version.json": "07e75b1547c018ef4806caf68e22a02e"};
 // The application shell files that are downloaded before a service worker can

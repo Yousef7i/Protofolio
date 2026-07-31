201a51414bd0800cc70dc9e42938d25c2553ebd1
Mohamed-Ghanem155 <promopcbig458@gmail.com>
2026-07-31 22:49:01 +0300

    fix: apply animated button style to all project links

--- flutter_web_build/flutter_service_worker.js
+++ flutter_web_build/flutter_service_worker.js
@@ -32,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d187f5194cc20f9ab6b41bfc0e8fe590
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "cv.pdf": "868930ed93a66ad03bd7de810a2dcdce",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ee193cb9831e524bd180fc556887e55f",
+"flutter_bootstrap.js": "76f848f2b8bd169115657f0b3bf96b85",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "2575e728eb872e4cf4bf87c382d9c26c",
 "/": "2575e728eb872e4cf4bf87c382d9c26c",
-"main.dart.js": "095d773e7aef3c80f37067dd4264d57a",
+"main.dart.js": "f55f5c9cfed46d5bc1847ca51190bbed",
 "manifest.json": "5e6899ef49e7405ac47ed4c97b6a6139",
 "version.json": "07e75b1547c018ef4806caf68e22a02e"};
 // The application shell files that are downloaded before a service worker can

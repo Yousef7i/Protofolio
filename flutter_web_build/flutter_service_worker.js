47a8608811e5a41a43f43f2302190f2bb820de14
Mohamed-Ghanem155 <promopcbig458@gmail.com>
2026-08-17 13:41:05 +0300

    feat: add profile photo as favicon + OG meta tags for link preview poster

--- flutter_web_build/flutter_service_worker.js
+++ flutter_web_build/flutter_service_worker.js
@@ -33,15 +33,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "fad81b2355d2b1de44894cad85ac8693
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "cv.pdf": "868930ed93a66ad03bd7de810a2dcdce",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "f7b07fe30266b9d783c810100994be37",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"flutter_bootstrap.js": "619e66c20f0fb9be32afa8aa1906ea37",
+"icons/Icon-192.png": "28d51bd854b1c1e58b6ae454fa3eb59a",
+"icons/Icon-512.png": "28d51bd854b1c1e58b6ae454fa3eb59a",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "11b64ef0bc554a8828b44cd4069e1b19",
-"/": "11b64ef0bc554a8828b44cd4069e1b19",
+"index.html": "29c3660a4daa71f0a573debbc134b1cf",
+"/": "29c3660a4daa71f0a573debbc134b1cf",
 "main.dart.js": "a397c9d746aca5c5e4951353c2781add",
 "manifest.json": "aa72e89e5da94ea347b43982be4eb673",
+"og-image.jpg": "28d51bd854b1c1e58b6ae454fa3eb59a",
 "version.json": "07e75b1547c018ef4806caf68e22a02e"};
 // The application shell files that are downloaded before a service worker can
 // start.
